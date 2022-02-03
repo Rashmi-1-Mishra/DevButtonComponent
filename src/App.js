0a912051001dfa2f126f640629b2a1b2871a76ce
Rashmi Mishra <mishrarashmi153@gmail.com>
2022-02-03 11:44:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SideComponent from './components/SideComponent';
+import RightComponent from './components/RightComponent';
+import FooterComponent from './components/FooterComponent';
+
+function App() {
+  return (
+    <div className="App">
+     <SideComponent />
+     <RightComponent />
+     <FooterComponent />
+    </div>
+  );
+}
+
+export default App;
